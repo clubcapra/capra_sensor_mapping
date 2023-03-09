a6331ac0aac8cdc53f5a7f2d632dfd99b10c4c36
Benoit Malenfant <benmalenfant@gmail.com>
2023-03-09 12:53:08 -0800

    Parametric tf frames

--- src/sensor_mapping.cpp
+++ src/sensor_mapping.cpp
@@ -6,6 +6,7 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <tf/transform_listener.h>
+#include <string>
 
 ros::Publisher pub;
 float sensor_data=-1;
@@ -20,6 +21,16 @@ int main (int argc, char** argv)
   ros::init (argc, argv, "capra_sensor_mapping");
   ros::NodeHandle nh;
 
+  std::string reference_frame = "map";
+  std::string sensor_frame = "base_link";
+
+  if(nh.getParam("reference_frame", reference_frame)==false){
+    ROS_WARN("Missing reference_frame Param, assuming map");
+  }
+  if(nh.getParam("sensor_frame", sensor_frame)==false){
+    ROS_WARN("Missing sensor_frame Param, assuming base_link");
+  }
+
   sensor_msgs::PointCloud2 output;
 
   pcl::PointCloud<pcl::PointXYZI> cloud;
@@ -27,13 +38,16 @@ int main (int argc, char** argv)
   cloud.height   = 1;
   cloud.is_dense = false;
   cloud.resize (cloud.width * cloud.height);
-  cloud.header.frame_id = "map";
+  cloud.header.frame_id = reference_frame;
+
 
   tf::TransformListener listener;
 
   pub = nh.advertise<sensor_msgs::PointCloud2>("output", 1);
 
   ros::Subscriber sub = nh.subscribe("sensor_input", 1, sensorCallback);
+
+  ros::Duration(1.0).sleep();
 
   ros::AsyncSpinner spinner(1);
   spinner.start();
@@ -43,7 +57,7 @@ int main (int argc, char** argv)
 
     tf::StampedTransform transform;
     try{
-      listener.lookupTransform("map", "base_link", ros::Time(0), transform);
+      listener.lookupTransform(reference_frame, sensor_frame, ros::Time(0), transform);
     }
     catch (tf::TransformException ex){
       ROS_ERROR("%s", ex.what());
