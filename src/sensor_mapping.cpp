4ecb281034f0786361aa16958fc54ae2506f6bd0
Benoit Malenfant <benmalenfant@gmail.com>
2023-03-09 12:23:35 -0800

    First working prototype - uses raw position evert second

--- src/sensor_mapping.cpp
+++ src/sensor_mapping.cpp
@@ -1,12 +1,18 @@
 #include <ros/ros.h>
 // PCL specific includes
 #include <sensor_msgs/PointCloud2.h>
+#include <std_msgs/Float64.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <tf/transform_listener.h>
 
 ros::Publisher pub;
+float sensor_data=-1;
+
+void sensorCallback(const std_msgs::Float64::ConstPtr& msg){
+  sensor_data = msg->data;
+}
 
 int main (int argc, char** argv)
 {
@@ -16,15 +22,23 @@ int main (int argc, char** argv)
 
   sensor_msgs::PointCloud2 output;
 
+  pcl::PointCloud<pcl::PointXYZI> cloud;
+  cloud.width    = 1;
+  cloud.height   = 1;
+  cloud.is_dense = false;
+  cloud.resize (cloud.width * cloud.height);
+  cloud.header.frame_id = "map";
+
   tf::TransformListener listener;
 
-  // Create a ROS publisher for the output point cloud
   pub = nh.advertise<sensor_msgs::PointCloud2>("output", 1);
+
+  ros::Subscriber sub = nh.subscribe("sensor_input", 1, sensorCallback);
 
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
-  ros::Rate rate(10.0);
+  ros::Rate rate(1000.0);
   while (ros::ok()){
 
     tf::StampedTransform transform;
@@ -37,24 +51,17 @@ int main (int argc, char** argv)
       continue;
     }
 
-    ROS_INFO("x: %.2f, y: %.2f, z: %.2f", transform.getOrigin().x(), transform.getOrigin().y(), transform.getOrigin().z());
+    pcl::PointXYZI point;
 
-    pcl::PointCloud<pcl::PointXYZI> cloud;
-    cloud.width    = 5;
-    cloud.height   = 1;
-    cloud.is_dense = false;
-    cloud.resize (cloud.width * cloud.height);
+    point.x=transform.getOrigin().x();
+    point.y=transform.getOrigin().y();
+    point.z=transform.getOrigin().z();
+    point.intensity=sensor_data;
 
-    for(int i = 0; i<5; i++){
-      cloud.points.at(i).x=i;
-      cloud.points.at(i).z=0;
-      cloud.points.at(i).y=0;
-      cloud.points.at(i).intensity=i;
-    }
-    cloud.header.frame_id = "map";
+    cloud.insert(cloud.end(), point);
+ 
     pcl::toROSMsg(cloud, output);
     pub.publish (output);
-    // Spin
     rate.sleep();
   }
 }
